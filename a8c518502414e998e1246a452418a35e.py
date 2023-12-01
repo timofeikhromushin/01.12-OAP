ce321ea176fde8655241dcda78067f4a7c09a125
Timofei Khromushin <114978343+timofeikhromushin@users.noreply.github.com>
2023-12-01 10:45:27 +0300

    Add files via upload

---
+++ a8c518502414e998e1246a452418a35e.py
@@ -0,0 +1,13 @@
+from fractions import Fraction
+
+a = int(input("Введите числитель первой дроби: "))
+b = int(input("Введите знаменатель первой дроби: "))
+c = int(input("Введите числитель второй дроби: "))
+d = int(input("Введите знаменатель второй дроби: "))
+
+frac1 = Fraction(a, b)
+frac2 = Fraction(c, d)
+
+result = frac1 - frac2
+
+print(f"Результат: {result.numerator}/{result.denominator}")
